1a2463bfd2cb4825ad5438880d152af3a512ed3c
YevsInna <evseinka@gmail.com>
2021-08-24 17:40:07 +0300

    lesson3

--- Node/lesson_3/controllers/user.controller.js
+++ Node/lesson_3/controllers/user.controller.js
@@ -1,8 +1,20 @@
+const db = require('../db/users');
+
 module.exports= {
     getSingleUser: (req, res) => {
+        const {user_id} = req.params
+        const user = db[user_id];
+
+        if (!user){
+            res.status(404).json('User is not found');
+            return
+        }
+        res.json(user);
     },
+
     getAllUsers: (req, res) => {
     },
+
     createUser: (req, res) => {
         res.json('Ok')
     },
--- Node/lesson_3/routers/index.js
+++ Node/lesson_3/routers/index.js
@@ -1,5 +1,5 @@
-module.exports={
+module.exports = {
     authRouter: require('./auth.router'),
     userRouter: require('./user.router')
 
-}
+};
